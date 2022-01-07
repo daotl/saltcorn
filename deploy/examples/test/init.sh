956f3d4da7e404ee2cf65cb4868d0cae0390deab
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2022-01-07 23:38:42 +0800

    Fork, modify docker-compose.yml: fix image tags, volumes, add scripts

---
+++ deploy/examples/test/init.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+VOLUMES_ROOT=/data/docker/volumes/saltcorn
+mkdir -p ${VOLUMES_ROOT}/postgres
---
+++ deploy/examples/test/up.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+VOLUMES_ROOT=/data/docker/volumes/saltcorn \
+docker compose up -d
